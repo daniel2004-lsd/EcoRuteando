040b101af0fee73bbd3ca31dbbe6338f326fa3f2
daniel <danielsalazarvargas953@gmail.com>
2026-04-10 18:40:02 -0500

    The translator functionality is currently only available in English, at least on the homepage view.

--- front-end/ecoRuteando-react/src/features/auth/components/Navbar.jsx
+++ front-end/ecoRuteando-react/src/features/auth/components/Navbar.jsx
@@ -2,13 +2,13 @@ import { useState } from "react";
 
 function Navbar() {
   const [language, setLanguage] = useState("es");
- // commit
+
   return (
-    <nav className="w-full p-4 flex justify-end bg-white shadow relative z-50">
+    <nav className="w-full flex justify-end items-center p-3 bg-white shadow-md sticky top-0 z-50">
       <select
         value={language}
         onChange={(e) => setLanguage(e.target.value)}
-        className="border rounded px-2 py-1"
+        className="border rounded px-2 py-1 text-sm sm:text-base"
       >
         <option value="es">ES</option>
         <option value="en">EN</option>
